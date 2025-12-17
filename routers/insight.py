ed0fa851f1ad1670b192bac48fb8ece1fe7a1bba
Joston Rodrigues <jostonrodrigues@Jostons-Macbook-Pro.local>
2025-12-17 12:55:23 -0500

    routers for insight. Untested

---
+++ routers/insight.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+from db import get_conn
+from schemas.insight import InsightCreate, InsightOut
+import psycopg2.extras
+from repositories.insight_repo import get_all_insights, \
+    get_insight_by_id, get_series_insights, get_source_insights, create_insight
+
+router = APIRouter(
+    prefix="/insight",
+    tags=["insight"]
+)
+
+@router.get("", response_model=List[InsightOut])
+def get_insights():
+    return get_all_insights()
+
+@router.get("/source/{source_id}", response_model=InsightOut)
+def source_insight(source_id: int):
+    return (get_source_insights(source_id))
+
+@router.get("/series/{series_id}", response_model=InsightOut)
+def series_insight(series_id: int):
+    return (get_series_insights(series_id))
+
+@router.get("/{insight_id}", response_model=InsightOut)
+def get_insight(insight_id: int):
+    return (get_insight_by_id(insight_id))
+
+@router.post("", response_model=InsightOut)
+def create_single_insight(payload: InsightCreate):
+    return (create_insight(payload))
